6df2e419bda8b8562d60223f3b80025e462fd139
Merdi <kandimerdi@gmail.com>
2025-08-25 12:27:38 +0100

    Secondary commit

---
+++ simple_backup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# ----------------------------------------------------------
+# Script name : simple_backup.sh
+# Author : Merdtheavatar
+# Date ; August 25, 2025
+# Description: Script to back up a MySQL/PostgreSQL database
+# License: MIT (Open Source)
+# ----------------------------------------------------------
+
+# Enter credentials
+read -p "Enter your database user : " db_user
+# read -s -p "Enter your database password : " db_password
+
+# Set space after credentials
+echo " "
+
+# Enter database type 
+read -p "Enter type database type (mysql/prostgres) : " db_type
+
+# Enter database name
+read -p "Enter database name : " db_name
+
+# Create database directory if not exist
+mkdir -p ~/db_backups
+
+# Generate database file with database name, date en sql extension
+db_file=~/db_backups/${db_name}_$(date +%F_%H-%M).sql
+
+if [[ "$db_type" == "mysql" ]]; then
+	# Make database dump
+	mysqldump -u "$db_user" "$db_name" > $db_file
+	if [[ $? -eq 0 ]]; then
+		echo "Backup successful ! : $db_file"
+	else
+		echo "An error has occurred"
+	fi
+elif
+	[[ "$db_type" == "postgres" || "$db_type" == "postgresql" ]]; then
+		
+	read -s -p "Enter database password: " pg_password
+
+	PGPASSWORD="$pg_password" pg_dump -U "$db_user" -h localhost "$db_name" > "$db_file"
+    	if [[ $? -eq 0 ]]; then
+        	echo "Backup successful ! : $db_file"
+    	else
+        	echo "An error has occurred"
+    	fi
+else
+	echo "unsupported database"
+fi
